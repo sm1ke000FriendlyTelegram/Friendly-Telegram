5a126342ebaed162532f3c1518cf41afed7afa43
sm1ke000FriendlyTelegram <78103559+sm1ke000FriendlyTelegram@users.noreply.github.com>
2021-02-01 12:53:03 +0500

    Upload file

---
+++ help.py
@@ -0,0 +1,81 @@
+﻿import logging, inspect
+from telethon.tl.functions.channels import JoinChannelRequest
+from .. import loader, utils, main, security
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class HelpMod(loader.Module):
+    """Provides this help message"""
+    strings = {"name": "Help",
+               "bad_module": "<b>Invalid module name specified</b>",
+               "single_mod_header": ("<b>Help for</b> <u>{}</u>:\nNote that the monospace text are the commands "
+                                     "and they can be run with <code>{}&lt;command&gt;</code>"),
+               "single_cmd": "\n• <u>{}</u>\n",
+               "undoc_cmd": "There is no documentation for this command",
+               "all_header": "<b>Available Modules <a href='https://t.me/FtgModules_int'>{}</a>:</b>",
+               "mod_tmpl": "\n• <b>{}</b>",
+               "first_cmd_tmpl": " {}",
+               "cmd_tmpl": " | {}"}
+
+    @loader.unrestricted
+    async def hcmd(self, message):
+        """.h [module]"""
+        args = utils.get_args_raw(message)
+        if args:
+            module = None
+            for mod in self.allmodules.modules:
+                if mod.strings("name", message).lower() == args.lower():
+                    module = mod
+            if module is None:
+                await utils.answer(message, self.strings("bad_module", message))
+                return
+            try:
+                name = module.strings("name", message)
+            except KeyError:
+                name = getattr(module, "name", "ERROR")
+            reply = self.strings("single_mod_header", message).format(utils.escape_html(name),
+                                                                      utils.escape_html((self.db.get(main.__name__,
+                                                                                                     "command_prefix",
+                                                                                                     False) or ".")[0]))
+            if module.__doc__:
+                reply += "\n" + "\n".join("  " + t for t in utils.escape_html(inspect.getdoc(module)).split("\n"))
+            else:
+                logger.warning("Module %s is missing docstring!", module)
+            commands = {name: func for name, func in module.commands.items()
+                        if await self.allmodules.check_security(message, func)}
+            for name, fun in commands.items():
+                reply += self.strings("single_cmd", message).format(name)
+                if fun.__doc__:
+                    reply += utils.escape_html("\n".join("  " + t for t in inspect.getdoc(fun).split("\n")))
+                else:
+                    reply += self.strings("undoc_cmd", message)
+        else:
+            count = 0
+            for modul in self.allmodules.modules:
+                if len(modul.commands) != 0:
+                    count += 1
+            reply = self.strings("all_header", message).format(count)
+            for mod in self.allmodules.modules:
+                try:
+                    name = mod.strings("name", message)
+                except KeyError:
+                    name = getattr(mod, "name", "ERROR")
+                reply += self.strings("mod_tmpl", message).format(name)
+                first = True
+                commands = [name for name, func in mod.commands.items()
+                            if await self.allmodules.check_security(message, func)]
+                for cmd in commands:
+                    if first:
+                        reply += self.strings("first_cmd_tmpl", message).format(f'({cmd}')
+                        first = False
+                    else:
+                        reply += self.strings("cmd_tmpl", message).format(cmd)
+                reply += ")"
+        await utils.answer(message, reply)
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.is_bot = await client.is_bot()
+        self.db = db
