22cc540a15ddacf79bbf0a38ebe20448992d73b4
Smoke <78103559+sm1ke000FriendlyTelegram@users.noreply.github.com>
2021-03-24 17:09:51 +0500

    Upload file

---
+++ GosuV2.py
@@ -0,0 +1,37 @@
+﻿#clown... again :/
+
+from asyncio import sleep
+import random
+from telethon import functions
+from userbot.events import register
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon import events
+from .. import loader, utils
+
+def register(cb):
+    cb(GosuMod())
+
+class GosuMod(loader.Module):
+    """ты еблан да?"""
+    strings = {'name': 'Госу от чебурека'}
+
+    def __init__(self):
+        self.name = self.strings['name']
+        self._me = None
+        self._ratelimit = []
+
+    async def client_ready(self, client, db):
+        self._db = db
+        self._client = client
+
+    async def gosucmd(self, event):
+        chat = '@gosu_by_im_dont_glad_bot'
+        async with event.client.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(events.NewMessage(incoming=True, from_users= 1762666746 ))
+                await event.client.send_message(chat, '/start')
+                response = await response
+            except YouBlockedUserError:
+                await event.edit('<code>Разблокируй @gosu_by_im_dont_glad_bot</code>')
+                return
+            await event.edit(response.text)
